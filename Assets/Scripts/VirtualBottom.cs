73969fac75176b1bed57ac2726bc25f3d3cf5645
DESKTOP-VMUPS6B\massa <hmmyfh@gmail.com>
2018-02-27 13:20:08 -0600

    commit

---
+++ Assets/Scripts/VirtualBottom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+public class VirtualBottom : MonoBehaviour,
+IVirtualButtonEventHandler
+{ 
+
+ public VirtualButtonBehaviour virtualBtn;
+  public Animator birdAnim;
+    public AudioSource sound;
+	// Use this for initialization
+	void Start () {
+        virtualBtn = GetComponent<VirtualButtonBehaviour>();
+        virtualBtn.RegisterEventHandler(this);
+    }
+ 
+
+        public void OnButtonPressed(VirtualButtonBehaviour vb)
+        {
+          //  throw new System.NotImplementedException();
+            birdAnim.SetTrigger("sing");
+            sound.Play();
+    }
+
+        public void OnButtonReleased(VirtualButtonBehaviour vb)
+        {
+            
+        }
+    }
+   
